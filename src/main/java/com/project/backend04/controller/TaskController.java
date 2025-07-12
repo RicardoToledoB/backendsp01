50d1a5f6e666bfa9f33e0402f63a662dfe0e5f9a
ricardotoledob <ricardo.toledob@gmail.com>
2025-07-12 02:40:37 -0300

    test

--- src/main/java/com/project/backend04/controller/TaskController.java
+++ src/main/java/com/project/backend04/controller/TaskController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class TaskController {
 
     @Autowired
